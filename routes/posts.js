57da77b8b3b665f0e8e17493ae0b50f713de475f
Victorafx <victor.auxietrefontes@laposte.net>
2020-06-15 09:57:12 +0200

    resolve express.router quest

---
+++ routes/posts.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fakePosts = require('../data/posts');
+const comments = require('./comments');
+
+const router = express.Router();
+
+router.use('/:postId/comments', comments);
+
+router.get('/', (req, res) => {
+res.json(fakePosts);
+});
+
+router.get('/:postId', (req, res) => {
+// Find the post in the array that has the id given by req.params.id
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+const postId = Number(req.params.id);
+const foundPost = fakePosts.find((post) => post.id === postId);
+if (!foundPost) {
+return res.status(404).json({
+error: 'Post not found',
+});
+}
+return res.json(foundPost);
+});
+
+module.exports = router;
